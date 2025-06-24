687be180e4541678cc1ef56b86687286fbe6d0e1
Sirawich-Kwan <sirawich5050@gmail.com>
2025-06-24 18:42:06 +0700

    Add files via upload

---
+++ cell01/ex02/age.py
@@ -0,0 +1,5 @@
+real_age = 19
+additional_number = 42
+my_age = real_age + additional_number
+print(my_age)
+"""Instruction: Your script will add your age and 42, and assign the result to a third variable called my_age."""
